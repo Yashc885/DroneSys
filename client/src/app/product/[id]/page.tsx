d81f136a5a06f2777a85cf9a3be52ea4ee79ff2b
Yashc885 <yashcoudhary88505342@gmail.com>
2024-08-05 18:37:52 +0530

    data changed

--- client/src/app/product/[id]/page.tsx
+++ client/src/app/product/[id]/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import Product from "../../components/Product/Product";
-import { NavbarSimple } from "../../components/Common/Navbar";
+import Product from "../../../components/Product/Product.jsx";
+import { NavbarSimple } from "../../../components/Common/Navbar";
 
 export default function Home() {
   return (
